9de321ac1690d9a5f0a228ec9b88189c550f9cc0
Kirill <paltsev.kir@gmail.com>
2023-09-17 19:05:29 +0400

    Update formula

--- qli.rb
+++ qli.rb
@@ -1,12 +1,23 @@
 class Qli < Formula
   desc "Qli is qase command line utility"
   homepage "https://github.com/qase-tms/qasectl"
-  url "https://github.com/qase-tms/qasectl/releases/download/v0.1.4/qli-v0.1.4-Linux-amd64.zip"
-  sha256 "36e35b7cb201353de1b85ef440a945d7b7ba6c3292a68df8c08160669a389e46"
   version "0.1.4"
   license "Apache License 2.0"
 
+  if Hardware::CPU.arm?
+    url "https://github.com/qase-tms/qasectl/releases/download/v#{version}/qli-v#{version}-macOS-arm64.zip"
+    sha256 "e18fbcb42772de965498839b859b46a58f7505c3ddd5aa2a9218ddda1774495c"
+  else
+    url "https://github.com/qase-tms/qasectl/releases/download/v#{version}/qli-v#{version}-macOS-amd64.zip"
+    sha256 "4765ba71a26e3028e1ccf77a6f8054cc18fc122b2aadb52c49f585c95dc5fb5a"
+  end
+
   def install
+    if Hardware::CPU.arm?
+      system "mv", "qli-v#{version}-macOS-arm64", "qli"
+    else
+      system "mv", "qli-v#{version}-macOS-amd64", "qli"
+    end
     bin.install "qli"
   end
 
